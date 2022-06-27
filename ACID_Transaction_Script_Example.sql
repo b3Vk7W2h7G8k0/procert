1115046e9be22fdd5c500534843cd84d500edfba
b3Vk7W2h7G8k0 <105084546+b3Vk7W2h7G8k0@users.noreply.github.com>
2022-06-27 17:11:43 +0100

    Add files via upload

---
+++ ACID_Transaction_Script_Example.sql
@@ -0,0 +1,106 @@
+/*ACID Transactions in SQL*/
+
+
+/*Examples:*/
+
+
+/*ACID Transaction begin with command with "BEGIN" se example below:*/
+
+DELIMITER //
+
+CREATE PROCEDURE TRANSACTION_ROSE()
+
+BEGIN
+
+   DECLARE EXIT HANDLER FOR SQLEXCEPTION
+   BEGIN
+       ROLLBACK;
+       RESIGNAL;
+   END;                
+
+     START TRANSACTION;
+       UPDATE BankAccounts
+       SET Balance = Balance-200
+       WHERE AccountName = 'Rose';
+
+       UPDATE BankAccounts
+       SET Balance = Balance+200
+       WHERE AccountName = 'Shoe Shop';
+
+       UPDATE ShoeShop
+       SET Stock = Stock-1
+       WHERE Product = 'Boots';
+
+       UPDATE BankAccounts
+       SET Balance = Balance-300
+       WHERE AccountName = 'Rose';
+
+
+
+       COMMIT;    
+
+END //
+
+DELIMITER ; 
+
+
+
+/*Calling Transactions*/
+
+    CALL TRANSACTION_ROSE; 
+
+    SELECT * FROM BankAccounts;
+
+    SELECT * FROM ShoeShop;
+	
+	
+/*Create a stored procedure TRANSACTION_JAMES to execute a transaction based on the following scenario: 
+First buy James 4 pairs of Trainers from ShoeShop. 
+Update his balance as well as the balance of ShoeShop. 
+Also, update the stock of Trainers at ShoeShop. 
+Then attempt to buy James a pair of Brogues from ShoeShop. 
+If any of the UPDATE statements fail, the whole transaction fails. 
+You will roll back the transaction. Commit the transaction only if the whole transaction is successful.*/
+
+
+DELIMITER //
+
+CREATE PROCEDURE TRANSACTION_JAMES()                         
+
+
+
+BEGIN
+
+
+
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        ROLLBACK;
+        RESIGNAL;
+    END;           
+        START TRANSACTION;
+        UPDATE BankAccounts
+        SET Balance = Balance-1200
+        WHERE AccountName = 'James';
+
+        UPDATE BankAccounts
+        SET Balance = Balance+1200
+        WHERE AccountName = 'Shoe Shop';
+
+        UPDATE ShoeShop
+        SET Stock = Stock-4
+        WHERE Product = 'Trainers';
+
+        UPDATE BankAccounts
+        SET Balance = Balance-150
+        WHERE AccountName = 'James';
+
+
+
+            COMMIT;
+
+
+
+END //
+
+DELIMITER ; 
